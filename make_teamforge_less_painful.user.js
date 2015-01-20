548eea7ac5765936d5d76da72c2000d6a23dd450
Will Sheppard <github@siteaboutnothing.com>
2015-01-20 10:23:17 +0000

    Neaten everything up, use #id, closest(), parent()

--- make_teamforge_less_painful.user.js
+++ make_teamforge_less_painful.user.js
@@ -1,42 +1,69 @@
 // ==UserScript==
 // @name        make-teamforge-less-painful
-// @namespace   http://compufer.com/
-// @version     0.2
+// @namespace   http://siteaboutnothing.com/javascript
+// @version     0.4
 // @include     https://www.dbcde.com/*
 // @description Hide unused buttons, and resize + relocate important buttons
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @grant       GM_addStyle
-// @copyright   2014+, Will Sheppard
+// @copyright   2014,2015+, Will Sheppard
 // ==/UserScript==
 
 /*
 TODOs:
-	* Remove whole .Buttons, not just 'a's
-	* Make 'Return' and 'Edit' .Buttons big too
-	* Make border go around .Middle, not .Button so it looks better
-	* Remove 'Users Monitoring' and 'Stop Monitoring' buttons
-  * Refactor the logic so it's properly modular
+	// How the hell do I copy the button next to the comment area?
+    // None of these work...
+    //$('textarea#comment').append('#viewArtifactForm_SaveAndView');
+	//$('#viewArtifactForm_SaveAndView').appendTo( 'textarea#comment' );
+	//$('#viewArtifactForm_SaveAndView').appendTo( $('textarea#comment') );
+	//$('#viewArtifactForm_SaveAndView').clone().appendTo('textarea#comment');
+    $('textarea#comment').css({"border":"3px solid red"}); // works
+
+    * Refactor the logic so there's less duplication
 */
 
 // Hide unwanted buttons
-var button_texts = ["Update", "Cancel"];
+var button_texts = [ "Update", "Cancel", "Users Monitoring", "Stop Monitoring" ]; // View/edit page
 button_texts.forEach(function(button_text) {
-    $('#viewArtifactForm .Button a').filter(function() {
+//    $('#viewArtifactForm .Button a').filter(function() {
+    $('.Button a').filter(function() {
         return $(this).text() == button_text;
-    }).hide();
+    }).closest('.Button').hide();
     console.log("Hiding button with text '"+button_text+"'");
 });
 
-// Make the only important button really big and bold
-$('#viewArtifactForm .AlignRight').filter(function() {
-    var importantButton = $(this).find(".Button").find("a").filter(function() {
-        return $(this).text() == "Update And View";
-    }); //.text();
-    importantButton.css({
-        "line-height":"835%",
-        "font-weight":"bold",
-        "font-size":"150%",
-        "border":"3px solid black"
-    });
-    return true;
-}).css({"float":"left"}); // ...and move it to the left
+var button_texts = [ "Cancel", "Save" ]; // New/create page
+button_texts.forEach(function(button_text) {
+    $('#createArtifactForm .Button a').filter(function() {
+        return $(this).text() == button_text;
+    }).closest('.Button').hide();
+    console.log("Hiding button with text '"+button_text+"'");
+});
+
+// "View Artefact" page
+// Make the update button really big and bold (View/edit page)
+$('#viewArtifactForm_SaveAndView').closest('.AlignRight').css({
+    "float": "left"
+});
+$('#viewArtifactForm_SaveAndView').css({
+    "line-height":	"835%",
+    "font-weight":	"bold",
+    "font-size":	"150%"
+});
+$('#viewArtifactForm_SaveAndView').closest('.Middle').css({
+    "border":		"3px solid black"
+});
+
+
+// "Submit Artefact" page
+$('#createArtifactForm_SaveAndView').closest('.AlignRight').css({
+    "float": "left"
+});
+$('#createArtifactForm_SaveAndView').css({
+    "line-height":	"835%",
+    "font-weight":	"bold",
+    "font-size":	"150%"
+});
+$('#createArtifactForm_SaveAndView').closest('.Middle').css({
+    "border":		"3px solid black"
+});
