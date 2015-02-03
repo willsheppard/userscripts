776d7b76316f40fa8e5bd6d18975bf0caa401ec9
Will Sheppard <github@siteaboutnothing.com>
2015-02-03 13:54:54 +0000

    Copy save button next to comment box

--- make_teamforge_less_painful.user.js
+++ make_teamforge_less_painful.user.js
@@ -1,29 +1,13 @@
 // ==UserScript==
 // @name        make-teamforge-less-painful
 // @namespace   http://siteaboutnothing.com/javascript
-// @version     0.4
+// @version     0.5
 // @include     https://www.dbcde.com/*
 // @description Hide unused buttons, and resize + relocate important buttons
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @grant       GM_addStyle
 // @copyright   2014,2015+, Will Sheppard
 // ==/UserScript==
-
-/*
-TODOs:
-    * Hide "Users Monitoring", "Stop Monitoring"
-       difficult as they have no named container
-     
-    * How the hell do I copy the button next to the comment area?
-      None of these work...
-    //$('textarea#comment').append('#viewArtifactForm_SaveAndView');
-    //$('#viewArtifactForm_SaveAndView').appendTo( 'textarea#comment' );
-    //$('#viewArtifactForm_SaveAndView').appendTo( $('textarea#comment') );
-    //$('#viewArtifactForm_SaveAndView').clone().appendTo('textarea#comment');
-    $('textarea#comment').css({"border":"3px solid red"}); // works
-
-    * Refactor the logic so there's less duplication
-*/
 
 // Hide unwanted buttons
 var button_texts = [ "Update", "Cancel" ]; // View/edit page
@@ -41,6 +25,8 @@ button_texts.forEach(function(button_text) {
     }).closest('.Button').hide();
     console.log("Hiding button with text '"+button_text+"'");
 });
+
+console.log("Embiggening buttons");
 
 // "View Artefact" page
 // Make the update button really big and bold (View/edit page)
@@ -69,3 +55,25 @@ $('#createArtifactForm_SaveAndView').css({
 $('#createArtifactForm_SaveAndView').closest('.Middle').css({
     "border":		"3px solid black"
 });
+
+console.log("Copying buttons");
+
+// Copy the save button next to the comment area
+var save_button = $('#viewArtifactForm_SaveAndView')
+	.closest('.Middle')
+	.clone()
+	.attr('id','#viewArtifactForm_SaveAndView2') // <-- not working?
+	.insertAfter("textarea#comment");
+
+/****************************************************************************/
+
+/*
+TODOs:
+	 * Hide "Users Monitoring", "Stop Monitoring"
+       difficult as they have no named container
+
+DEBUG:
+//$('textarea#comment').css({"border":"3px solid red"});
+//$('#viewArtifactForm_SaveAndView').closest('.Middle').css({"border":"3px solid green"});
+*/
+
