965d84d42c884ec17a83ebc9fe3e7f0c21e06496
Will Sheppard <github@siteaboutnothing.com>
2015-05-21 10:35:08 +0100

    Move Comment Text box left + more
    Unfortunately all the line endings also changed

--- make_teamforge_less_painful.user.js
+++ make_teamforge_less_painful.user.js
@@ -1,13 +1,42 @@
 // ==UserScript==
 // @name        make-teamforge-less-painful
 // @namespace   http://siteaboutnothing.com/javascript
-// @version     0.5
+// @version     0.7
 // @include     https://www.dbcde.com/*
 // @description Hide unused buttons, and resize + relocate important buttons
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @grant       GM_addStyle
 // @copyright   2014,2015+, Will Sheppard
 // ==/UserScript==
+
+// Ensure errors are always noticed. Without this, all the error messages
+// are below the fold and can be missed, meaning you think you've updated
+// the artefact but really you haven't!
+// Bug: This error can match on other pages unexpectedly
+// Bug: It doesn't catch page-timeouts
+if ($('.errorText').length) {
+    // Add a warning message where it should be
+    var warningDiv = jQuery('<div/>', {
+        id: 'Sf_MessageId_fake',
+        class: 'redText',
+        title: 'Warning: There were errors',
+        text: 'Warning: Artefact not saved due to errors, see below.',
+        display: 'block'
+    });
+    warningDiv.css({
+        'color': '#B94A48', // dark red
+        'background-color': '#FFEFE7', // light red
+        padding: '15px',
+        border: '3px solid #B94A48',
+        'margin-top': '10px'
+    });
+    warningDiv.appendTo('.breadcrumb');
+
+    // Make absolutely sure the user takes notice
+    //alert("Warning: Form not submitted - see errors below")
+
+    console.log("Ensuring errors are noticed");
+}
 
 // Hide unwanted buttons
 var button_texts = [ "Update", "Cancel" ]; // View/edit page
@@ -63,9 +92,28 @@ var save_button = $('#viewArtifactForm_SaveAndView')
 	.closest('.Middle')
 	.clone()
 	.attr('id','#viewArtifactForm_SaveAndView2') // <-- not working?
-	.insertAfter("textarea#comment");
+    ; // .css({ "float": "right"});
 
-/****************************************************************************/
+save_button.insertAfter("textarea#comment");
+
+$("textarea#comment").css({ "float": "left" });
+
+// TODO:
+// Make the cloned button look the same as the old one
+// And actually submit the original button
+// Bug: Button doesn't work exactly right
+/*
+save_button.css({
+    "float": 	  "right",
+    "font-color": "black",
+    "font-decoration": "none"
+});
+*/
+
+// Move the 'Comment Text' to the left
+console.log("Moving comments box left");
+$('#artifactSummarySection > tbody > tr > td:nth-child(1)').css({ "width": "5%" });
+//$('#artifactSummarySection > tbody > tr > td:nth-child(2)').css({ "width": "5%" });
 
 /*
 TODOs:
@@ -76,4 +124,3 @@ DEBUG:
 //$('textarea#comment').css({"border":"3px solid red"});
 //$('#viewArtifactForm_SaveAndView').closest('.Middle').css({"border":"3px solid green"});
 */
-
