0987f3d4298ee8664c54cc44d2425fd586e8ca2a
Will Sheppard <nobody@example.com>
2017-11-21 16:31:12 +0000

    Update more for NAP

--- tweak_jenkins.user.js
+++ tweak_jenkins.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        tweak-jenkins
-// @namespace   http://example.com/jenkins
+// @namespace   http://compufer.com/jenkins
 // @version     0.1
-// @include     https://example.com/*
+// @include     https://jenkins16.wtf.nap/*
 // @description Tweaks
 // @require     http://code.jquery.com/jquery-2.1.4.min.js
 // @require     http://code.jquery.com/ui/1.11.4/jquery-ui.min.js
@@ -18,6 +18,7 @@ $("li a").filter(function(index) { return $(this).text() === "Jenkins"; }).appen
 $('a').filter(function(index) { return $(this).text() === "Delete Project"; }).parent('.task').hide();
 
 // Make "build" link bigger because I click it all the time
+/*
 $('a').filter(function(index) { return $(this).text() === "Build Now"; }).css({
     'font-weight': 'bold',
     'font-size':   '300%',
@@ -25,8 +26,9 @@ $('a').filter(function(index) { return $(this).text() === "Build Now"; }).css({
     'padding':     '6px',
     'margin':      '40px'
 });
+*/
 
-// Make a clearer separation between promotion jobs
+// Promotions: Make a clearer separation between jobs
 //    if page URL has /promotion in it
 $('h2').filter(function(index) {
     var regex = new RegExp('/promotion');
@@ -34,7 +36,24 @@ $('h2').filter(function(index) {
 }).css({
     'border-top': '2px solid black',
     'padding-top': '10px',
-    'border-left': '2px solid',
-    'border-right': '2px solid',
+    'border-left': '2px solid black',
+    'border-right': '2px solid black',
     'padding-left': '10px'
 });
+
+// Current builds: Divide the rows more clearly
+$('.build-row').filter(function(index) {
+    var regex = new RegExp('/view');
+    return regex.test(window.location.href);
+}).css({
+    'border-top': '1px solid grey',
+});
+
+// Current builds: Get rid of the grey lozenge
+$('.build-row.multi-line .build-row-cell .block').css({ 'overflow': 'visible' });
+
+// Promotions: Disable the "Force promotion" button
+// Warning: Although this greys them out, they may not actually be disabled
+$('button').filter(function(index) {
+    return $(this).text() === "Force promotion";
+}).attr('disabled', 'disabled');
