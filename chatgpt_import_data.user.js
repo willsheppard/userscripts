c4ee1b4de4aebd5312ba79a3485dfc8ab03d9a4d
Will Sheppard <1519594+willsheppard@users.noreply.github.com>
2023-06-29 20:13:13 +0100

    Create chatgpt_import_data.user.js

---
+++ chatgpt_import_data.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://chat.openai.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @require      https://cdn.jsdelivr.net/gh/chatgptjs/chatgpt.js@1e3e24998c2a338b4db8f28cc01c3e8e6bc9e872/dist/chatgpt-1.10.4.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
